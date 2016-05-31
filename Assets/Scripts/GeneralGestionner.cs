4fff67c88e3fb65f1d5580c9bdd51d893d9413dc
GuillaumeJulien <Guillaume.Julien@etu.univ-savoie.fr>
2016-05-31 13:58:40 +0200

    VuForia
    Assets Réalité Augmenté

---
+++ Assets/Scripts/GeneralGestionner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GeneralGestionner : MonoBehaviour {
+    private Scene activeScene;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+    public void LoadScene(string scene)
+    {
+        activeScene = SceneManager.GetActiveScene();
+        SceneManager.UnloadScene(activeScene.name);
+        SceneManager.LoadScene(scene);
+    }
+}
