efbca8b1a1e01f4738db13f77a0a434f217144c7
GuillaumeJulien <Guillaume.Julien@etu.univ-savoie.fr>
2016-05-31 13:17:33 +0200

    Initial Commit

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour
+{
+    public RectTransform myRectTrans;
+    public float movementScale;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+    
+
+    }
+}
